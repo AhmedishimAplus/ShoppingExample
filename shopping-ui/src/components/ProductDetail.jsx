05139a32dd6e202c800b05f87d3d9ce951e677f6
AhmedishimAplus <atamerfawzy@gmail.com>
2024-11-21 15:52:38 +0200

    component projectdetails

---
+++ shopping-ui/src/components/ProductDetail.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { getProductById } from '../services/api';
+const ProductDetail=()=>{
+    const [product,setProduct]=useState(null);
+    useEffect(()=> {getProductById(id).then((response) => setProduct(response.data));}
+        ,[id]);
+    getProductById(id).then((response) => setProduct(response.data));
+    return (<div>
+        <h2>{product.name}</h2>
+        <p>Price: ${product.price}</p>
+        <p>Description: {product.description}</p>
+    </div>
+    );
+}
+export default ProductDetail;
