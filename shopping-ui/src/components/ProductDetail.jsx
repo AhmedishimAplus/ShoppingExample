331824307bc0af7f2e488cad5721afb27b4f3298
AhmedishimAplus <atamerfawzy@gmail.com>
2024-11-21 16:20:45 +0200

    forgot to add use params in product detail

--- shopping-ui/src/components/ProductDetail.jsx
+++ shopping-ui/src/components/ProductDetail.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getProductById } from '../services/api';
 const ProductDetail=()=>{
+    const { id } = useParams();
     const [product,setProduct]=useState(null);
     useEffect(()=> {getProductById(id).then((response) => setProduct(response.data));}
         ,[id]);
